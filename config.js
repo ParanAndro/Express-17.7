8cc08109160cf5365786fb7fa82693e14466dc3f
Radek Izdebski <radek1izdebski@gmail.com>
2018-04-15 12:34:25 +0200

    Add views and server.js

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports ={
+	'GOOGLE_CLIENT_ID': '997683196627-rgp1g9ebjnd95a6ibdneob80b0ugicdl.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': 'SypeSv55Z4jD76IFEJ4mqkDy',
+	'CALLBACK_URL': 'http://localhost:3005/auth/google/callback'
+};
